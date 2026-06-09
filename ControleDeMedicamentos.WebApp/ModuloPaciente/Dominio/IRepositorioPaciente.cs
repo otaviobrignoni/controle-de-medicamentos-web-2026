ad670873484dda5a1ad2ae7c18f9cf6d17e089f1
Michel <michelgabiatti07@gmail.com>
2026-06-09 19:45:57 -0300

    Implementação de dóminio paciente

---
+++ ControleDeMedicamentos.WebApp/ModuloPaciente/Dominio/IRepositorioPaciente.cs
@@ -0,0 +1,8 @@
+using ControleDeMedicamentos.WebApp.Compartilhado.ModuloBase;
+
+namespace ControleDeMedicamentos.WebApp.ModuloPaciente.Dominio;
+
+public interface IRepositorioPaciente : IRepositorio<Paciente>
+{
+
+}
---
+++ ControleDeMedicamentos.WebApp/ModuloPaciente/Dominio/Paciente.cs
@@ -0,0 +1,32 @@
+using ControleDeMedicamentos.WebApp.Compartilhado.ModuloBase;
+
+namespace ControleDeMedicamentos.WebApp.ModuloPaciente.Dominio;
+
+public class Paciente : EntidadeBase<Paciente>
+{
+    public string Nome { get; set; } = string.Empty;
+    public string Telefone { get; set; } = string.Empty;
+    public string CartaoSUS { get; set; } = string.Empty;
+    public string CPF { get; set; } = string.Empty;
+
+    public Paciente
+    (
+        string nome,
+        string telefone,
+        string cartaoSUS,
+        string CPF
+    )
+    {
+        Nome = nome;
+        Telefone = telefone;
+        CartaoSUS = cartaoSUS;
+        this.CPF = CPF;
+    }
+    public override void Atualizar(Paciente entidadeAtualizada)
+    {
+        Nome = entidadeAtualizada.Nome;
+        Telefone = entidadeAtualizada.Telefone;
+        CartaoSUS = entidadeAtualizada.CartaoSUS;
+        CPF = entidadeAtualizada.CPF;
+    }
+}
