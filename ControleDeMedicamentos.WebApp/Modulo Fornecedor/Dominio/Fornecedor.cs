74a3f00feb1242fa7ecb5133d23d2d178b5d151a
Michel <michelgabiatti07@gmail.com>
2026-06-09 14:53:12 -0300

    Implementacao de Dominio Fornecedor

---
+++ ControleDeMedicamentos.WebApp/Modulo Fornecedor/Dominio/Fornecedor.cs
@@ -0,0 +1,26 @@
+using ControleDeMedicamentos.WebApp.Compartilhado.ModuloBase;
+
+namespace ControleDeMedicamentos.WebApp.Modulo_Fornecedor.Dominio;
+
+public class Fornecedor : EntidadeBase<Fornecedor>
+{
+    public string Nome { get; set; } = string.Empty;
+    public string Telefone { get; set; } = string.Empty;
+    public string CNPJ { get; set; } = string.Empty;
+
+    public Fornecedor() { }
+
+    public Fornecedor(string nome, string telefone, string cnpj)
+    {
+        Nome = nome;
+        Telefone = telefone;
+        CNPJ = cnpj;
+    }
+
+    public override void Atualizar(Fornecedor entidadeAtualizada)
+    {
+        Nome = entidadeAtualizada.Nome;
+        Telefone = entidadeAtualizada.Telefone;
+        CNPJ = entidadeAtualizada.Telefone;
+    }
+}
---
+++ ControleDeMedicamentos.WebApp/Modulo Fornecedor/Dominio/IRepositorioFornecedor.cs
@@ -0,0 +1,8 @@
+using ControleDeMedicamentos.WebApp.Compartilhado.ModuloBase;
+
+namespace ControleDeMedicamentos.WebApp.Modulo_Fornecedor.Dominio;
+
+public interface IRepositorioFornecedor : IRepositorio<Fornecedor>
+{
+
+}
