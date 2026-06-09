2dd71317eb1841117ab60baa74e40dc28cb66a78
Michel <michelgabiatti07@gmail.com>
2026-06-09 20:35:05 -0300

    Implementado Paciente Controller

--- ControleDeMedicamentos.WebApp/ModuloPaciente/Apresentacao/PacienteController.cs
+++ ControleDeMedicamentos.WebApp/ModuloPaciente/Apresentacao/PacienteController.cs
@@ -1,12 +1,125 @@
 using AutoMapper;
+using ControleDeMedicamentos.WebApp.Compartilhado.Extensions;
 using ControleDeMedicamentos.WebApp.ModuloPaciente.Aplicacao;
+using FluentResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ControleDeMedicamentos.WebApp.ModuloPaciente.Apresentacao
 {
     public class PacienteController(ServicoPaciente servicoPaciente, IMapper mapeador) : Controller
     {
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            var dtos = servicoPaciente.SelecionarTodos();
 
+            var ListarVms = mapeador.Map<List<PacienteViewModel>>(dtos);
 
+            return View(ListarVms);
+        }
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            PacienteViewModel vm = new(string.Empty, string.Empty, string.Empty, string.Empty);
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(PacienteViewModel vm)
+        {
+            if (!ModelState.IsValid)
+                return View(vm);
+
+            var dto = mapeador.Map<PacienteDto>(vm);
+
+            Result resultado = servicoPaciente.Cadastrar(dto);
+
+            if (resultado.IsFailed)
+            {
+                ModelState.AddModelError(resultado);
+
+                return View(vm);
+            }
+
+            return RedirectToAction(nameof(Listar));
+        }
+
+        [HttpGet]
+        public ActionResult Editar(Guid id)
+        {
+            var resultado = servicoPaciente.SelecionarPorId(id);
+
+            if (resultado.IsFailed)
+            {
+                TempData.AddErrorMessage(resultado);
+
+                return RedirectToAction(nameof(Listar));
+            }
+
+            var dto = resultado.Value;
+
+            var vm = mapeador.Map<PacienteViewModel>(dto);
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(PacienteViewModel vm)
+        {
+            if (!ModelState.IsValid)
+                return View(vm);
+
+            var dto = mapeador.Map<PacienteDto>(vm);
+
+            var resultado = servicoPaciente.Editar(dto);
+
+            if (resultado.IsFailed)
+            {
+                ModelState.AddModelError(resultado);
+
+                return View(vm);
+            }
+
+            return RedirectToAction(nameof(Listar));
+        }
+
+        [HttpGet]
+        public ActionResult Excluir(Guid id)
+        {
+            var resultado = servicoPaciente.SelecionarPorId(id);
+
+            if (resultado.IsFailed)
+            {
+                TempData.AddErrorMessage(resultado);
+
+                return RedirectToAction(nameof(Listar));
+            }
+
+            var dto = resultado.Value;
+
+            var vm = mapeador.Map<PacienteViewModel>(dto);
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public ActionResult Excluir(PacienteViewModel vm)
+        {
+            if (ModelState.IsValid)
+                return View(vm);
+
+            var resultado = servicoPaciente.Excluir(vm.Id);
+
+            if (resultado.IsFailed)
+            {
+                ModelState.AddModelError(resultado);
+
+                return View(vm);
+            }
+
+            return RedirectToAction(nameof(Listar));
+        }
     }
 }
