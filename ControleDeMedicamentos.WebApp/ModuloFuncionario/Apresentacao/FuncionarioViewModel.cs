230eb6872de8ead0e77492760c4455fe4bb926f5
Michel <michelgabiatti07@gmail.com>
2026-06-10 18:19:28 -0300

    Padronizacao do regex do campo CPF

--- ControleDeMedicamentos.WebApp/ModuloFuncionario/Apresentacao/FuncionarioViewModel.cs
+++ ControleDeMedicamentos.WebApp/ModuloFuncionario/Apresentacao/FuncionarioViewModel.cs
@@ -6,14 +6,14 @@ public record FuncionarioViewModel(
     [Required(ErrorMessage = "O campo \"Nome\" deve ser preenchido")]
     [StringLength(100, MinimumLength = 3, ErrorMessage = "O campo \"Nome\" deve conter entre 3 e 100 caracteres.")]
     string Nome,
-    
+
     [Required(ErrorMessage = "O campo \"Telefone\" é obrigatório.")]
     [RegularExpression(@"^\(\d{2}\)\s(9?\d{4})-\d{4}$", ErrorMessage = "O campo \"Telefone\" deve estar no formato (XX) XXXX-XXXX ou (XX) 9XXXX-XXXX)")]
     string Telefone,
-    
+
     [Required(ErrorMessage = "O campo \"CPF\" é obrigatório.")]
-    [RegularExpression(@"^[0-9]{11}$", ErrorMessage = "O campo \"CPF\" deve conter 11 dígitos")]
+    [RegularExpression(@"^\d{3}\.\d{3}\.\d{3}-\d{2}$", ErrorMessage = "O campo \"CPF\" deve conter 11 dígitos")]
     string Cpf,
-    
+
     Guid Id = default
 );
