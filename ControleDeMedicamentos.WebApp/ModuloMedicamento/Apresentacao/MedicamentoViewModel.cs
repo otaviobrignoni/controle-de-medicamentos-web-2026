c21903219766d109284ceef57cb1a39d314489be
Michel <michelgabiatti07@gmail.com>
2026-06-09 23:18:07 -0300

    Implementação do modelo MedicamentoViewModel e suas opções de fornecedores

---
+++ ControleDeMedicamentos.WebApp/ModuloMedicamento/Apresentacao/MedicamentoViewModel.cs
@@ -0,0 +1,43 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace ControleDeMedicamentos.WebApp.ModuloMedicamento.Apresentacao;
+
+public record MedicamentoViewModel
+(
+    [Required(ErrorMessage = "O campo \"Nome\" deve ser preenchido")]
+    [StringLength(100, MinimumLength =3, ErrorMessage =" O campo \"Nome\" deve conter entre 3 a 100 digitos")]
+    string Nome,
+
+    [Required(ErrorMessage = "O campo \"Descrição\" deve ser preenchido")]
+    [StringLength(255, MinimumLength =5, ErrorMessage =" O campo \"Descrição\" deve conter entre 5 a 255 digitos")]
+    string Descricao,
+
+    int Quantidade,
+
+    [Required(ErrorMessage = "O campo \"Categoria\" deve ser preenchido")]
+    Guid CategoriaId,
+
+    [ValidateNever]
+    OpcoesFornecedores Fornecedores,
+
+    Guid Id = new Guid()
+);
+public record MedicamentoMostrarViewModel
+(
+    string Nome,
+
+    string Descricao,
+
+    int Quantidade,
+
+    string CategoriaNome,
+
+    Guid Id = new Guid()
+);
+public record OpcoesFornecedores
+(
+    Guid Id,
+    string Nome
+);
