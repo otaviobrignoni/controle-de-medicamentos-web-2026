b7119f068959caba23b4889d7cce24c8e2d7b703
Michel <michelgabiatti07@gmail.com>
2026-06-10 15:27:36 -0300

    Adicionado extensões de strings

--- ControleDeMedicamentos.WebApp/Compartilhado/Extensions/StringExtensions.cs
+++ ControleDeMedicamentos.WebApp/Compartilhado/Extensions/StringExtensions.cs
@@ -11,4 +11,15 @@ public static class StringExtensions
 
         return char.ToUpper(texto[0], CultureInfo.CurrentCulture) + texto[1..];
     }
+
+    public static string Truncar(this string texto, int largura)
+    {
+        if (largura <= 0) return string.Empty;
+        if (largura == 1) return "…";
+
+        if (texto.Length <= largura)
+            return texto;
+
+        return texto[..(largura - 1)] + "…";
+    }
 }
