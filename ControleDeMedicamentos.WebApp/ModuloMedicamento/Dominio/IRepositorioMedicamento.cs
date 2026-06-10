87719451643f74ea6cacc095e1820fe9e2497524
Michel <michelgabiatti07@gmail.com>
2026-06-09 22:07:33 -0300

    Implementação de dominio

---
+++ ControleDeMedicamentos.WebApp/ModuloMedicamento/Dominio/IRepositorioMedicamento.cs
@@ -0,0 +1,8 @@
+using ControleDeMedicamentos.WebApp.Compartilhado.ModuloBase;
+
+namespace ControleDeMedicamentos.WebApp.ModuloMedicamento.Dominio;
+
+public interface IRepositorioMedicamento : IRepositorio<Medicamento>
+{
+
+}
---
+++ ControleDeMedicamentos.WebApp/ModuloMedicamento/Dominio/Medicamento.cs
@@ -0,0 +1,27 @@
+using ControleDeMedicamentos.WebApp.Compartilhado.ModuloBase;
+using ControleDeMedicamentos.WebApp.ModuloFornecedor.Dominio;
+
+namespace ControleDeMedicamentos.WebApp.ModuloMedicamento.Dominio;
+
+public class Medicamento : EntidadeBase<Medicamento>
+{
+    public string Nome { get; set; } = string.Empty;
+    public string Descricao { get; set; } = string.Empty;
+    public int Quantidade { get; set; } = 0;
+    public Fornecedor Fornecedor { get; set; } = new Fornecedor();
+
+    public Medicamento(string nome, string descricao, int quantidade, Fornecedor fornecedor)
+    {
+        Nome = nome;
+        Descricao = descricao;
+        Quantidade = quantidade;
+        Fornecedor = fornecedor;
+    }
+    public override void Atualizar(Medicamento entidadeAtualizada)
+    {
+        Nome = entidadeAtualizada.Nome;
+        Descricao = entidadeAtualizada.Descricao;
+        Quantidade = entidadeAtualizada.Quantidade;
+        Fornecedor = entidadeAtualizada.Fornecedor;
+    }
+}
